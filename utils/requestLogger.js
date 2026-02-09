b8dc1d07409f2d823a1024992473d4ec68f007f4
Ubuntu <ubuntu@ip-172-31-38-22.eu-north-1.compute.internal>
2026-02-09 20:32:26 +0000

    Add CloudWatch logging with Winston and structured JSON logs

---
+++ utils/requestLogger.js
@@ -0,0 +1,23 @@
+const logger = require('./logger');
+
+const requestLogger = (req, res, next) => {
+  const start = Date.now();
+
+  // Log when response finishes
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+
+    logger.info('API Request', {
+      method: req.method,
+      url: req.originalUrl,
+      status: res.statusCode,
+      duration: `${duration}ms`,
+      ip: req.ip || req.connection.remoteAddress,
+      userAgent: req.get('user-agent')
+    });
+  });
+
+  next();
+};
+
+module.exports = requestLogger;
