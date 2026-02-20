08f28fe3e64eac67c6d7b9199777e4d20cc982c1
oltir06 <ol871rex@htwg-konstanz.de>
2026-02-20 20:09:17 +0100

    Upgrade to Node 22, switch bcrypt to bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 const db = require('../db/db');
 const logger = require('../utils/logger');
