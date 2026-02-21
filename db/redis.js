e9ab6003894988140e97289a9b330bbca185ec0d
oltir06 <ol871rex@htwg-konstanz.de>
2026-02-21 21:04:50 +0000

    Add Redis client with ioredis and connection event logging

---
+++ db/redis.js
@@ -0,0 +1,37 @@
+const Redis = require('ioredis');
+const logger = require('../utils/logger');
+
+// Create Redis client
+const redis = new Redis({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
+  retryStrategy: (times) => {
+    const delay = Math.min(times * 50, 2000);
+    return delay;
+  },
+  maxRetriesPerRequest: 3
+});
+
+// Connection events
+redis.on('connect', () => {
+  logger.info('Redis client connected');
+});
+
+redis.on('ready', () => {
+  logger.info('Redis client ready');
+});
+
+redis.on('error', (err) => {
+  logger.error('Redis client error', { error: err.message });
+});
+
+redis.on('close', () => {
+  logger.warn('Redis client connection closed');
+});
+
+redis.on('reconnecting', () => {
+  logger.info('Redis client reconnecting');
+});
+
+module.exports = redis;
